88cd678c22e69639c1d5a3f6a0fa1a0a9e4cea9b
AnnieKapur <annikakapur1996@gmail.com>
2025-11-20 10:34:34 +0000

    Fuction for adding books and checking library

--- library.py
+++ library.py
@@ -16,3 +16,16 @@ def add_new_book_to_library():
     name = input("Enter the book name: ")
     quantity = int(input("Enter the quantity(num): "))
     library[isbn] = { "name": name, "quantity" : quantity }
+
+#if the title is already in the library, it needs to be added to the quantity
+#if/else to check for title 
+#if it is in the library, add it to the quantity that is already there
+#else, create a new entry in the dictionary (square brackets)
+#the new entry (else) will be = and not += because it is a new entry
+
+def add_new_book_to_library (library, title, quantity): 
+    if title in library:
+        library[title]+=quantity
+    else:
+        library[title] = quantity 
+    return library 
