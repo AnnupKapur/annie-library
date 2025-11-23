35bacbf89f9747810f8866e479899335f491748c
AnnupKapur <AnnupKapur@gmail.com>
2025-11-23 10:44:51 +0000

    add new books complete

--- library.py
+++ library.py
@@ -13,19 +13,11 @@ def get_book_name(isbn):
 # change this function to handle books we already have
 def add_new_book_to_library():
     isbn = input("Enter the ISBN : ")
-    name = input("Enter the book name: ")
-    quantity = int(input("Enter the quantity(num): "))
-    library[isbn] = { "name": name, "quantity" : quantity }
-
-#if the title is already in the library, it needs to be added to the quantity
-#if/else to check for title 
-#if it is in the library, add it to the quantity that is already there
-#else, create a new entry in the dictionary (square brackets)
-#the new entry (else) will be = and not += because it is a new entry
-
-def add_new_book_to_library (library, title, quantity): 
-    if title in library:
-        library[title]+=quantity
+    if isbn in library:
+        purchase_quantity_quantity = int(input("Enter the quantity(num): "))
+        library[isbn]["quantity"] += purchase_quantity
     else:
-        library[title] = quantity 
-    return library 
+        new_book_name = input("Enter the book name: ")
+        new_quantity = int(input("Enter the quantity(num): "))
+        library[isbn] = { "name": name, "quantity" : new_quantity }
+
