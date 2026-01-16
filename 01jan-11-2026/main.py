5a0d6089e1bcf4498f9981b20d1f671b48a65837
AnnieKapur <annikakapur1996@gmail.com>
2026-01-16 10:18:51 +0000

    Dial Homework

--- 01jan-11-2026/main.py
+++ 01jan-11-2026/main.py
@@ -1,7 +1,26 @@
 def main():
+    start_position = 50
+    count_of_zeros = 0
+
     with open("./input.txt") as file:
         for line in file:
+            line = line.strip()
+            if not line:
+                continue
+
             direction = line[0]
-            amount = line[1:]
+            amount = int(line[1:])
+
+            if direction == "R":
+                start_position = (start_position + amount) % 100
+            elif direction == "L":
+                start_position = (start_position - amount) % 100
+
+            if start_position == 0:
+                count_of_zeros += 1
+
+    print(count_of_zeros)
+
 
 main()
+
