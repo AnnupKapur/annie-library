83c636d0d1b872d393b3dbf001c6708cca14e6fa
AnnieKapur <annikakapur1996@gmail.com>
2026-01-21 10:26:21 +0000

    Invalid ID Homework

--- 01jan-18-2026/main.py
+++ 01jan-18-2026/main.py
@@ -2,6 +2,19 @@ def main():
 
     with open("./input.txt") as file:
         for line in file:
-            # your code
-
+            number_ranges = line.strip().split(',')
+            for item in number_ranges: 
+                numbers = item.split('-')
+                start = int(numbers[0])
+                end = int(numbers[1])
+            for every_number in range (start, end +1):
+                string = str(every_number)
+                length = len(string)
+            if length % 2 == 0:
+                mid = length // 2
+                first_half = string[:mid]
+                second_half = string[mid:]
+            if first_half == second_half: 
+                total = total + every_number
+            print("Answer:", total)
 main()
